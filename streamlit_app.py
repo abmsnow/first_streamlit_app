f444d50a98b9372df3bb7723ef4effcac9d3fb14
abmsnow <148498005+abmsnow@users.noreply.github.com>
2023-10-26 15:23:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorite')
-streamlit.text(':soup: Omega 3 & Bluerberry Oatmeal')
+streamlit.text('Omega 3 & Bluerberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
