537b6a48ae5516aa5a06872193673a88f4c8ac49
abmsnow <148498005+abmsnow@users.noreply.github.com>
2023-10-27 10:15:02 -0400

    Update streamlit_app.py
    We want the customer to be able to choose the fruits by name!!

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,15 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 ## Importing pandas
-
 import pandas
 
 ## We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+##  We want the customer to be able to choose the fruits by name!!
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 ## Add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
-
 ## Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
