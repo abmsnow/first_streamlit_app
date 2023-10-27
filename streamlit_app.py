df4e173d6846c310001b58565ab59d8b8777c5c4
abmsnow <148498005+abmsnow@users.noreply.github.com>
2023-10-27 10:05:24 -0400

    Update streamlit_app.py
    pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
    
    ask the streamlit library to display it on the page by typing:

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+## First Excercise
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
@@ -9,3 +10,14 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+## Importing pandas
+
+import pandas
+
+## We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+## ask the streamlit library to display it on the page by typing:
+streamlit.dataframe(my_fruit_list)
